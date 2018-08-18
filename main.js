abda4e3400f93cbbdb79366237e46160a3608cc3
Megaemce <mateuszosinski@o2.pl>
2018-08-19 00:24:31 +0200

    new technical details drawing system
    put the technical guard bool from main function into pointAtLength

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// ostatnia wersja 1154
-'use strict'
 let coords = [];
 let drawFirst = true;
 const draw = SVG('drawing').size(600, 600);
@@ -27,7 +25,7 @@ function drawLine(pointA, pointB, optionalColor) {
   });
 }
 function animateLine(pointA, pointB) {}
-function pointAtLength(ratio, pointA, pointB) {
+function pointAtLength(ratio, pointA, pointB, techDrawBool) {
   // returning point C on |AB| where |AC| = ratio*|AB|
   // circle function on point A: (x - aX)^2 + (y - aY)^2 = (ratio*|AB|)^2
   // function crossing through A i B: y = funAVal * x + funBVal
@@ -44,15 +42,17 @@ function pointAtLength(ratio, pointA, pointB) {
   // vertical line is not a function thus normal calucation cannot be made
   if (aX == bX) {
     if (aY > bY) {
-      intersections[0] = {
-        x: aX,
-        y: aY - rad
+      intersections[0] = {x: aX, y: aY - rad}
+      if (techDrawBool) {
+        drawCircle(intersections[0], 2, 'green');
+        drawCircle(pointA, rad);
       }
       return intersections[0];
     } else {
-      intersections[0] = {
-        x: aX,
-        y: aY + rad
+      intersections[0] = {x: aX, y: aY + rad}
+      if (techDrawBool) {
+        drawCircle(intersections[0], 2, 'green');
+        drawCircle(pointA, rad);
       }
       return intersections[0];
     }
@@ -63,60 +63,47 @@ function pointAtLength(ratio, pointA, pointB) {
     const solutionX0 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
     const solutionX1 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
 
-    intersections[0] = {
-    x: solutionX0,
-    y: funAVal * solutionX0 + funBVal
-    }
-    intersections[1] = {
-    x: solutionX1,
-    y: funAVal * solutionX1 + funBVal
-    }
+    intersections[0] = {x: solutionX0,y: funAVal * solutionX0 + funBVal}
+    intersections[1] = {x: solutionX1,y: funAVal * solutionX1 + funBVal}
 
     // check which intersection point is closed to point B
     if (segmentLength(intersections[0], pointB) < segmentLength(intersections[1], pointB)) {
-    return intersections[0];
+      if (techDrawBool) {
+        drawCircle(intersections[0], 2, 'green');
+        drawCircle(pointA, rad);
+      }
+      return intersections[0];
     } else {
-    return intersections[1];
+      if (techDrawBool) {
+        drawCircle(intersections[1], 2, 'green');
+        drawCircle(pointA, rad);
+      }
+      return intersections[1];
     }
   }
 }
 function segmentLength(pointA, pointB) {
   return Math.sqrt(Math.pow((pointA.x - pointB.x), 2) + Math.pow((pointA.y - pointB.y), 2))
 }
-function makeDrawing(box, techDraw, ratio, limit) {
+function makeDrawing(box, ratio, limit, techDrawBool) {
   let point = {};
 
   point[0] = box[0];                               // break starting in point A 
-  point[1] = pointAtLength(ratio, box[1], box[2]); // break on BC
-  point[2] = pointAtLength(ratio, box[2], box[3]); // break on CD 
-  point[3] = pointAtLength(ratio, box[3], box[0]); // break on DA
-
-  if (techDraw) {
-    box[4] = box[0];
-    for (let i = 0; i < 4; i++){
-      drawCircle(box[i], ratio * segmentLength(box[i], box[i+1]));
-      drawCircle(box[i], 2, 'blue');
-      drawCircle(point[i], 2, 'green');
-    }
-  }
+  point[1] = pointAtLength(ratio, box[1], box[2], techDrawBool); // break on BC
+  point[2] = pointAtLength(ratio, box[2], box[3], techDrawBool); // break on CD 
+  point[3] = pointAtLength(ratio, box[3], box[0], techDrawBool); // break on DA
 
   for (let i = 0; i < limit; i++) {
-    let interPoint = pointAtLength(ratio, point[i], point[i + 1]);
-    let lineL = segmentLength(point[i], point[i + 1]);
-    
-    if (i < 4) {lineL = 1;}
-    if (techDraw) {
-      drawCircle(point[i], ratio * lineL);
-      drawCircle(interPoint, 3, 'red');
-    }
+    let interPoint = pointAtLength(ratio, point[i], point[i + 1], techDrawBool);
+   
     point[Object.keys(point).length] = interPoint;
     drawLine(point[i], point[i + 1], i);
   }
 }
-function createBox(e) {
+function createBox(event) {
   if (coords.length < 4) {
-    coords.push({x: e.clientX, y: e.clientY});
-    drawCircle({x: e.clientX, y: e.clientY}, 2, 'black');
+    coords.push({x: event.clientX, y: event.clientY});
+    drawCircle({x: event.clientX, y: event.clientY}, 2, 'black');
   }
   if ((coords.length > 1) && (coords.length < 4)) {
     drawLine(coords[coords.length-2], coords[coords.length-1]);
@@ -126,9 +113,9 @@ function createBox(e) {
     drawLine(coords[3], coords[0]);
     makeDrawing(
 /* list of points taken from user */ coords, 
-/* draw technical details boolean */ 0,
-/* reflection length ratio [0-1] */  0.05,
-/* number of reflection  [0-inf] */  240
+/* reflection length ratio [0-1] */  0.2,
+/* number of reflection  [0-inf] */  200,
+/* draw technical details boolean */ 1
     )
     drawFirst = false;
   }
