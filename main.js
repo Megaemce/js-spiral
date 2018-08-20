d679e14e98c144753443848736fbf52ee20c647e
Megaemce <mateuszosinski@o2.pl>
2018-08-20 23:29:50 +0200

    code changed to canvas
    no svg library needed anymore. Animation added. Now the box can be with more than 4 points.

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
+let numPoints = 4;
 let coords = [];
 let drawFirst = true;
-const draw = SVG('drawing').size(600, 600);
+
+const canvas = document.getElementById('tutorial');
+const ctx = canvas.getContext('2d');
 document.body.addEventListener('click', createBox);
 
 function drawCircle(point, rad, optionalColor) {
-  if (arguments.length == 2) {
-    optionalColor = 'none';
-  }
-  draw.circle(rad * 2).stroke({
-    width: 0.2,
-    dasharray: 2
-  }).fill(`${optionalColor}`).move(point.x - rad, point.y - rad);
+  ctx.beginPath();
+  ctx.arc(point.x, point.y, rad, 0, 2 * Math.PI);
+  ctx.strokeStyle = 'black';
+  ctx.stroke();
+  if (arguments.length == 3) {
+    ctx.fillStyle = optionalColor;
+    ctx.fill();
+  }  
 }
 function drawLine(pointA, pointB, optionalColor) {
   if (arguments.length == 2) {
@@ -18,13 +22,42 @@ function drawLine(pointA, pointB, optionalColor) {
   } else {
     optionalColor = `hsl(${80+optionalColor*4.35}, 100%, 45%)`
   }
-  draw.path(`M${pointA.x} ${pointA.y}L${pointB.x} ${pointB.y}`
-  ).stroke({
-    width: 0.5,
-    color: optionalColor
-  });
+  
+  ctx.beginPath();
+  ctx.moveTo(pointA.x, pointA.y);
+  ctx.lineTo(pointB.x, pointB.y);
+  //ctx.setLineDash([5,10])
+  //ctx.lineWidth = '0.5';
+  ctx.strokeStyle = optionalColor;
+  ctx.stroke();
 }
-function animateLine(pointA, pointB) {}
+function animate(points, granular) {
+    let waypoints = [];
+    let t = 1;
+    // create (100/i) points in between two points
+    for (let i = 1; i < points.length; i++) {
+        const dx = points[i].x - points[i - 1].x;
+        const dy = points[i].y - points[i - 1].y;
+        for (let j = 0; j < granular; j++) {
+            const x = points[i - 1].x + (dx * j) / granular;
+            const y = points[i - 1].y + (dy * j) / granular;
+            waypoints.push({
+                x: x,
+                y: y
+            });
+        }
+    } // animate the drawing
+    
+    setInterval(function() {
+      if (t < waypoints.length) {
+        drawLine(waypoints[t - 1], waypoints[t], t)
+        t++;
+      }
+    }, 1)
+}
+function segmentLength(pointA, pointB) {
+  return Math.sqrt(Math.pow((pointA.x - pointB.x), 2) + Math.pow((pointA.y - pointB.y), 2))
+}
 function pointAtLength(ratio, pointA, pointB, techDrawBool) {
   // returning point C on |AB| where |AC| = ratio*|AB|
   // circle function on point A: (x - aX)^2 + (y - aY)^2 = (ratio*|AB|)^2
@@ -63,8 +96,8 @@ function pointAtLength(ratio, pointA, pointB, techDrawBool) {
     const solutionX0 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
     const solutionX1 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
 
-    intersections[0] = {x: solutionX0,y: funAVal * solutionX0 + funBVal}
-    intersections[1] = {x: solutionX1,y: funAVal * solutionX1 + funBVal}
+    intersections[0] = {x: solutionX0, y: funAVal * solutionX0 + funBVal}
+    intersections[1] = {x: solutionX1, y: funAVal * solutionX1 + funBVal}
 
     // check which intersection point is closed to point B
     if (segmentLength(intersections[0], pointB) < segmentLength(intersections[1], pointB)) {
@@ -82,39 +115,40 @@ function pointAtLength(ratio, pointA, pointB, techDrawBool) {
     }
   }
 }
-function segmentLength(pointA, pointB) {
-  return Math.sqrt(Math.pow((pointA.x - pointB.x), 2) + Math.pow((pointA.y - pointB.y), 2))
-}
 function makeDrawing(box, ratio, limit, techDrawBool) {
   let point = [];
-
-  point[0] = box[0];                               // break starting in point A 
-  point[1] = pointAtLength(ratio, box[1], box[2], techDrawBool); // break on BC
-  point[2] = pointAtLength(ratio, box[2], box[3], techDrawBool); // break on CD 
-  point[3] = pointAtLength(ratio, box[3], box[0], techDrawBool); // break on DA
+  // reflections points at the "box"
+  point[0] = box[0]
+  for (let i = 1; i < box.length - 1; i++){
+		point[i] = pointAtLength(ratio, box[i], box[i+1], techDrawBool);
+  }
+  point[box.length-1] = pointAtLength(ratio, box[box.length-1], box[0], techDrawBool);
 
   for (let i = 0; i < limit; i++) {
     point.push(pointAtLength(ratio, point[i], point[i + 1], techDrawBool));
-    drawLine(point[i], point[i + 1], i);
   }
+  
+  animate(point, Math.round(ratio*100));
 }
 function createBox(event) {
-  if (coords.length < 4) {
+  if (coords.length < numPoints) {
     coords.push({x: event.clientX, y: event.clientY});
     drawCircle({x: event.clientX, y: event.clientY}, 2, 'black');
   }
-  if ((coords.length > 1) && (coords.length < 4)) {
-    drawLine(coords[coords.length-2], coords[coords.length-1]);
+  if ((coords.length > 1) && (coords.length < numPoints)) {
+    drawLine(coords[coords.length - 2], coords[coords.length - 1]);
   }
-  if ((coords.length == 4) && drawFirst) {
-    drawLine(coords[2], coords[3]);
-    drawLine(coords[3], coords[0]);
+  if ((coords.length == numPoints) && drawFirst) {
+    drawFirst = false;
+    for (let i = 2; i < numPoints - 1; i++) {
+      drawLine(coords[i], coords[i + 1]);
+    }
+    drawLine(coords[numPoints - 1], coords[0]);
     makeDrawing(
 /* list of points taken from user */ coords, 
-/* reflection length ratio [0-1] */  0.2,
-/* number of reflection  [0-inf] */  200,
-/* draw technical details boolean */ 1
-    )
-    drawFirst = false;
+/* reflection length ratio [0-1] */  0.15,
+/* number of reflection  [0-inf] */  80,
+/* draw technical details boolean */ 0
+    );   
   }
 }
